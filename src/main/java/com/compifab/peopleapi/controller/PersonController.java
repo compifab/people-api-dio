9ff99f6d6bdbaf4ba195668127c3c74269ffcbf0
Fabricio Souza <compifab@gmail.com>
2021-04-18 18:27:54 -0300

    Create readme file.

--- src/main/java/com/compifab/peopleapi/controller/PersonController.java
+++ src/main/java/com/compifab/peopleapi/controller/PersonController.java
@@ -4,6 +4,8 @@ import com.compifab.peopleapi.dto.MessageResponseDTO;
 import com.compifab.peopleapi.dto.request.PersonDTO;
 import com.compifab.peopleapi.exception.PersonNotFoundException;
 import com.compifab.peopleapi.service.PersonService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    public PersonController(PersonService PersonService) {
-        this.personService = PersonService;
-    }
 
     @GetMapping
     public List<PersonDTO> listAll() {
