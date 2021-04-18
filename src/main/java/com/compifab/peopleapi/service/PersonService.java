f820ded39683faa40002f85333bc29d95fd41013
Fabricio Souza <compifab@gmail.com>
2021-04-18 18:19:17 -0300

    Update person method.

--- src/main/java/com/compifab/peopleapi/service/PersonService.java
+++ src/main/java/com/compifab/peopleapi/service/PersonService.java
@@ -24,10 +24,7 @@ public class PersonService {
         Person savedPerson = personMapper.toModel(personDTO);
         personRepository.save(savedPerson);
 
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID: " + savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "Created person with ID: ");
     }
 
     public List<PersonDTO> listAll() {
@@ -46,8 +43,24 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person updatedPerson = personMapper.toModel(personDTO);
+        personRepository.save(updatedPerson);
+
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID: ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 }
