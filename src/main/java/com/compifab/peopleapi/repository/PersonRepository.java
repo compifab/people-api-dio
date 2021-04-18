241a276e67707a7e4ddb9a7f8c936da8268e6729
Fabricio Souza <compifab@gmail.com>
2021-04-18 13:57:35 -0300

    Create PersonRepository.

--- src/main/java/com/compifab/peopleapi/repository/PersonRepository.java
+++ src/main/java/com/compifab/peopleapi/repository/PersonRepository.java
@@ -1,2 +1,7 @@
-package com.compifab.peopleapi.repository;public interface PersonRepository {
+package com.compifab.peopleapi.repository;
+
+import com.compifab.peopleapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
