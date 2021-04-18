00a1782dbba428297b7f39b79aef8fb43453c8a1
Fabricio Souza <compifab@gmail.com>
2021-04-18 14:20:48 -0300

    Create method createPerson.

--- src/main/java/com/compifab/peopleapi/controller/PersonController.java
+++ src/main/java/com/compifab/peopleapi/controller/PersonController.java
@@ -1,15 +1,29 @@
 package com.compifab.peopleapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.compifab.peopleapi.dto.MessageResponseDTO;
+import com.compifab.peopleapi.entity.Person;
+import com.compifab.peopleapi.service.PersonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
+    private PersonService personService;
+
+    public PersonController(PersonService PersonService) {
+        this.personService = PersonService;
+    }
+
     @GetMapping
     public String getBook() {
         return "Hello my best friend!";
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
